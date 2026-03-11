467733923e7a676034e2cde49c2d7c24b53dd577
rohan <rohansharmas028@gmail.com>
2026-03-11 17:47:37 +0530

    chore: rebuild and deploy after user changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -73,7 +73,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="icon" href="/logo.svg" type="image/svg+xml" />
       </head>
       <body className={inter.className}>
         <ReduxProvider>
